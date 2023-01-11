b3c186c432a428941d320c2607908bfcf7ae7a63
David <popadavid843@yahoo.com>
2023-01-11 14:11:14 +0200

    update

--- models/carModel.js
+++ models/carModel.js
@@ -72,81 +72,81 @@ const carSchema = new mongoose.Schema({
   slug: String,
   fuelTypes: {
     type: String,
-    // required: [true, 'Must contains a fuel type'],
+    required: [true, 'Must contains a fuel type'],
     enums: ['petrol', 'electric', 'lpg', 'hybrid', 'diesel'],
     default: 'petrol',
   },
   transmissionTypes: {
     type: String,
-    // required: [true, 'Must contains a transmission type'],
+    required: [true, 'Must contains a transmission type'],
     enums: ['automatic', 'manual'],
     default: 'automatic',
   },
   kilometers: {
     type: Number,
-    // required: [true, 'Must contains a kilometer number'],
+    required: [true, 'Must contains a kilometer number'],
   },
-  // firstRegistr: {
-  //   type: Number,
-  //   required: [true, 'Must contains a register data'],
-  // },
-  // power: {
-  //   type: Number,
-  //   required: [true, 'Must contains a power number'],
-  // },
-  // bodyType: {
-  //   type: String,
-  //   required: [true, 'Must contains a body type'],
-  //   enums: ['cabrio', 'combi', 'compact', 'coupe', 'suv'],
-  // },
-  // color: {
-  //   type: String,
-  // },
-  // originCountry: {
-  //   type: String,
-  //   required: [true, 'Must contains a country name'],
-  // },
-  // seller: {
-  //   type: String,
-  //   required: [true, 'Must contains a seller name'],
-  //   enums: ['private', 'company'],
-  // },
-  // emission: {
-  //   type: String,
-  // },
-  // numbersDoors: {
-  //   type: Number,
-  //   required: [true, 'Must contains a number of doors'],
-  // },
-  // taxi: {
-  //   type: String,
-  //   required: [true, 'Must contains if it was a taxi or not'],
-  //   default: 'No',
-  // },
-  // damage: {
-  //   type: String,
-  //   required: [true, 'Must contains if it had a damage'],
-  //   default: 'No',
-  // },
-  // summary: {
-  //   type: String,
-  //   required: [true, 'Must contains a summary'],
-  //   trim: true,
-  // },
-  // price: {
-  //   type: Number,
-  //   required: [true, 'Must contains a price'],
-  // },
-  // createdAt: {
-  //   type: Date,
-  //   default: Date.now(),
-  // },
-  // validFrom: {
-  //   type: Date,
-  // },
-  // validTo: {
-  //   type: Date,
-  // },
+  firstRegistr: {
+    type: Number,
+    required: [true, 'Must contains a register data'],
+  },
+  power: {
+    type: Number,
+    required: [true, 'Must contains a power number'],
+  },
+  bodyType: {
+    type: String,
+    required: [true, 'Must contains a body type'],
+    enums: ['cabrio', 'combi', 'compact', 'coupe', 'suv'],
+  },
+  color: {
+    type: String,
+  },
+  originCountry: {
+    type: String,
+    required: [true, 'Must contains a country name'],
+  },
+  seller: {
+    type: String,
+    required: [true, 'Must contains a seller name'],
+    enums: ['private', 'company'],
+  },
+  emission: {
+    type: String,
+  },
+  numbersDoors: {
+    type: Number,
+    required: [true, 'Must contains a number of doors'],
+  },
+  taxi: {
+    type: String,
+    required: [true, 'Must contains if it was a taxi or not'],
+    default: 'No',
+  },
+  damage: {
+    type: String,
+    required: [true, 'Must contains if it had a damage'],
+    default: 'No',
+  },
+  summary: {
+    type: String,
+    required: [true, 'Must contains a summary'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Must contains a price'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  validFrom: {
+    type: Date,
+  },
+  validTo: {
+    type: Date,
+  },
   images: [String],
 });
 
